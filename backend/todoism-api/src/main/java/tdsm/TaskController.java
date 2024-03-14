7140f4efee7d90e3f54a0fab585478564532dd8b
archloner <filipbiernat@gmail.com>
2024-03-14 10:34:30 +0100

    fix: try to debug enpoint not available through browser running in wsl

--- backend/todoism-api/src/main/java/tdsm/TaskController.java
+++ backend/todoism-api/src/main/java/tdsm/TaskController.java
@@ -1,11 +1,14 @@
 package tdsm;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import tdsm.domain.Task;
 
+@ComponentScan
 @RestController
 @RequestMapping(path = "/api", produces = "application/json")
 public class TaskController {
@@ -20,9 +23,9 @@ public class TaskController {
     public TaskRepository taskRepo;
 
     @GetMapping("/task")
-    public Iterable<Task> findAllTasks() {
+    public ResponseEntity<Iterable<Task>> findAllTasks() {
         TaskControllerLogger.log.info("Returning list of all tasks in TaskController");
-        return taskRepo.findAll();
+        return ResponseEntity.ok(taskRepo.findAll());
     }
 
 }
