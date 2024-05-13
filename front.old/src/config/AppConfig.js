64c4a932369116f71c0beed5c2dea5a42b9252e1
philbjern <filipbiernat@gmail.com>
2024-05-13 17:05:42 +0200

    fix: remove legacy front end app and rename current front app directory

--- front.old/src/config/AppConfig.js
+++
@@ -1,8 +0,0 @@
-export class AppConfig {
-  static DARK_MODE_DEFAULT = false;
-  static LOAD_WITH_SAMPLE_DATA = true;
-  static DEFAULT_PROJECT_ID = 0;
-  static LOCAL_STORAGE_KEY = "todo-data";
-  static NOTIFICATION_HIDE_ANIMATION_DURATION = 500;
-  static NOTIFICATION_SHOW_TIME = 4000;
-}
--- front.old/src/entity/PriorityType.js
+++
@@ -1,6 +0,0 @@
-export class PriorityType {
-  static LOW = 'low';
-  static MEDIUM = 'medium';
-  static HIGH = 'high';
-  static DEFAULT = 'default';
-}
