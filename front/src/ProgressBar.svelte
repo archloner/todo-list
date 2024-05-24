11744829c430470bce262d7418edbdfe048d5867
philbjern <filipbiernat@gmail.com>
2024-05-24 14:54:49 +0200

    feat: add confetti

--- front/src/ProgressBar.svelte
+++ front/src/ProgressBar.svelte
@@ -1,4 +1,5 @@
 <script>
+  import confetti from 'canvas-confetti'; 
   let progressBar;
   let complete;
   let taskcount;
@@ -10,6 +11,9 @@
 
     let percent = completed / alltasks;
     setWidthPercent(percent * 100);
+    if (complete == taskcount) {
+      confetti({particleCount: 120, spread: 150});
+    }
   }
 
   export function setWidthPercent(width) {
@@ -18,7 +22,7 @@
 </script>
 
 <div class="progress-bar-wrapper">
-  <div class="counter">{complete}/{taskcount}</div>
+  <div class="counter">{complete} of {taskcount} complete</div>
   <div class="red-bar">
     <div class="progress-bar" bind:this={progressBar}></div>
   </div>
@@ -31,17 +35,29 @@
   }
 
   .counter {
-    padding: 0.5em;
-    font-size: 1.6rem;
+    padding: 0.5em 0.5em 0.5em 0;
+    font-size: 1.3rem;
   }
 
   .progress-bar {
     width: 50%;
     background-color: lightgreen;
-    border-radius: 5px;
+    border-radius: 5px 0 0 5px;
     height: 10px;
     transition: all 0.3s ease-in-out;
+    position: relative;
   }
+/* 
+  .progress-bar::after {
+    content: '';
+    width: 7px;
+    height: 7px;
+    background-color: lightgreen;
+    transform: rotate(45deg);
+    right: -4px;
+    top: 1.5px;
+    position: absolute;
+  } */
 
   .red-bar {
     background-color: lightcoral;
